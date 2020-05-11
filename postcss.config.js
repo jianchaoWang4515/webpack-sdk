b83bf6db2460970c54c0141c252b9d3a9c9419a8
jianchao.wang <jianchao.wang@geely.com>
2020-05-11 16:40:53 +0800

    feat: 跟目录下可使用postcss.config.js文件

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,16 @@
+const postConfig = require(process.cwd() + '/postcss.config.js');
 /**
  * 增加浏览器前缀
  */
-module.exports = {
+
+const baseConfig = {
     plugins: [
         require('autoprefixer')({
             browsers: ['last 30 versions', "> 2%", "Firefox >= 10", "ie >= 9"]
         })
     ]
+}
+module.exports = {
+    ...baseConfig,
+    ...postConfig
 };
